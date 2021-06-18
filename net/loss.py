edd716d622cd3e6047302ad2c3e51f66ba33d519
javadba <javadba@gmail.com>
2021-06-18 11:03:29 -0700

    hardcode loss weights to .32//68

--- net/loss.py
+++ net/loss.py
@@ -1,8 +1,9 @@
 import torch.nn.functional as F
+import torch
 
 def nll_loss(output, target):
     # loss for log_softmax
-    return F.nll_loss(output, target)
+    return F.nll_loss(output, target, weight=torch.tensor([0.32, 0.68]))
 
 def cross_entropy(output, target):
     return F.cross_entropy(output, target)
